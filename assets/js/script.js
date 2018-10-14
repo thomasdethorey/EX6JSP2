238110e70325efa5fd8719f5a78fd0843da5996e
Thomas Dethorey <thomas.dethorey01@gmail.com>
2018-10-14 15:53:49 +0200

    ex 6

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+function changementimage(img){
+document.getElementById('image1').src='assets/img/image1_2.jpg';
+document.getElementById('image2').src='assets/img/image2_2.jpg';
+document.getElementById('image3').src='assets/img/image3_2.jpg';
+document.getElementById('image4').src='assets/img/image4_2.jpg';
+document.getElementById('image5').src='assets/img/image5_2.jpg';
+}
+function  changementimage2(img){
+  document.getElementById('image1').src='assets/img/image1.jpg';
+  document.getElementById('image2').src='assets/img/image2.jpg';
+  document.getElementById('image3').src='assets/img/image3.jpg';
+  document.getElementById('image4').src='assets/img/image4.jpg';
+  document.getElementById('image5').src='assets/img/image5.jpg';
+}
